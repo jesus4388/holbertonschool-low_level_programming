c091142af9d1f01295efd5578c52fadb8ec48d55
root <jesus4388>
2022-02-03 14:23:42 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ *main - main
+ *
+ *return: always 0
+ *
+ *descripcion: mi primer proyecto en c
+ *
+ */
+int main(void)
+{
+printf("printf(with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
