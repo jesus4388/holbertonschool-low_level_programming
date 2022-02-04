4b4a871af8a9546add761fa81416ea7fbfad494a
root <jesus4388>
2022-02-04 13:05:02 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,17 @@
 int main(void)
 
 {
-char ALPHABET = 'A';
-while (ALPHABET <= 'Z')
-{
-putchar(ALPHABET);
-ALPHABET = ALPHABET + 1;
-}
 char alphabet = 'a';
 while (alphabet <= 'z')
 {
 putchar(alphabet);
 alphabet = alphabet + 1;
+}
+char ALPHABET = 'A';
+while (ALPHABET <= 'Z')
+{
+putchar(ALPHABET);
+ALPHABET = ALPHABET + 1;
 }
 putchar('\n');
 return (0);
