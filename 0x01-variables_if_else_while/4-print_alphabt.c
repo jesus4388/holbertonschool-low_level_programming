677dada37ad18e34d5feb65681dcc501bb0f34ce
root <jesus4388>
2022-02-04 14:35:20 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ */
+
+int main(void)
+{
+
+char lt = 'a';
+ 
+while (lt <= 'z')
+{
+if (lt != 'q') || (lt != 'e')
+putchar(lt);
+lt++;
+}
+putchar('\n');
+return (0);
+}
