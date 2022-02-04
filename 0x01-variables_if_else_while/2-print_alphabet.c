6cb4a9db2047022f0063f940dc4360865e5f88ca
root <jesus4388>
2022-02-04 11:19:20 -0800

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: The last digit
+ *
+ */
+int main(void)
+
+{
+char Mm = 'a';
+while (Mm <= 'z')
+{
+putchar(Mm);
+
+Mm = Mm + 1;
+
+putchar('\n');
+}
+ return (0);
+}
