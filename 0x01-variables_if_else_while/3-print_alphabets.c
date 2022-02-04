15b9c401c4e510cbe164e6b58fd14081d373b92a
root <jesus4388>
2022-02-04 12:35:57 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: alphabet
+ *
+ */
+int main(void)
+
+{
+char ALPHABET = 'A';
+while (ALPHABET = 'Z')
+{
+putchar(ALPHABET);
+ALPHABET = ALPHABET + 1;
+}
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet = alphabet + 1;
+}
+putchar('\n');
+return (0);
+}
