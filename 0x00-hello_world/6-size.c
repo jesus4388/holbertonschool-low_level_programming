2b566ff24f0ef8978e628d121e0c64495b211e72
root <jesus4388>
2022-02-03 18:18:55 -0800

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ *
+ * main - main
+ * Return: 0
+ *
+ * descripcion: mi primer proyecto en c
+ *
+ */
+int main(void)
+{
+char tipochar;
+int ent;
+long int lint;
+long long llint;
+float fl;
+
+printf("Size of a char: %lu byte(s)\n", (sizeof(char)));
+printf("Size of an int: %lu byte(s)\n", (sizeof(int)));
+printf("Size of a long int: %lu byte(s)\n", (sizeof(lint)));
+printf("Size of a long long int: %lu byte(s)\n", (sizeof(llint)));
+printf("Size of a float: %lu byte(s)\n", (sizeof(fl)));
+return (0);
+}
