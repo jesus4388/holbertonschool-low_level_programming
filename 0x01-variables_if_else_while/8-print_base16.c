c91c014ecf6d3f0d547733a9970f5f706ca091fa
root <jesus4388>
2022-02-04 17:10:05 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: numbers
+ *
+ */
+
+int main(void)
+
+{
+
+char a;
+
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);  
+}
