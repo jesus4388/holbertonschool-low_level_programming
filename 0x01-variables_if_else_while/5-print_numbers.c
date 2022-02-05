b23445a7881c7a30273b7489c11e957d93e442a7
root <jesus4388>
2022-02-04 16:11:33 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * description:  print numbers
+ *
+ */
+
+int main(void)
+
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
