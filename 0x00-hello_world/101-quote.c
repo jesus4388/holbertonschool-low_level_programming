9fa93147b407640f96614e4db2772fdad559e563
root <jesus4388>
2022-02-05 05:58:26 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
  * main - main
  * Return: 1
  *
- *decripcion: mi primer proyecto
+ * decription: my first project c
  *
  */
-int main (void)
+int main(void)
+
 {
-  write(2, "
-and that piece of art is useful" - Dora Korpar, 2015-10-19\n" 59):
-return (1)
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
