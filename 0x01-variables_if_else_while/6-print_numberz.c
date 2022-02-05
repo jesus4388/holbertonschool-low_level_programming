ad4166d8735b40f53fbea28a764bb067af57b494
root <jesus4388>
2022-02-04 16:25:00 -0800

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: print numberz
+ *
+ */
+
+int main(void)
+
+{
+char a;
+for (a = 'z'; a >= 'a'; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
