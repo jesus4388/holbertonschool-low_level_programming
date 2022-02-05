b7e020530cf2ad3ddd8c289bdfd201dbe73dd40b
root <jesus4388>
2022-02-04 17:00:42 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ *
+ * description: print tebahpla
+ *
+ */
+
+int main(void)
+{
+
+char a;
+
+for (a = 'z'; a >= 'a'; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
