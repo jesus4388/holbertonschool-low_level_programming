7f85355e142a7ca9317559dcb5a8d3dd1f21c485
root <jesus4388>
2022-02-07 09:31:49 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-#
+#include "main.h"
+/**
+ * print_alphabet - - description: alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
