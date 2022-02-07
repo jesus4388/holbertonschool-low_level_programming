3ad8b1ae0fda001f4de75ea1082cd3cf3904f1d3
root <jesus4388>
2022-02-07 15:41:21 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - - description: calculate the absolute value of a whole number
+ * @x: absolute value
+ * Return: x basolute value
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+	x = x * -1;
+	}
+
+	return (x);
+}
