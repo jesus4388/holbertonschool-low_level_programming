ab2faa124f80228552550686964559eba2b04f33
root <jesus4388>
2022-02-07 07:33:32 -0800

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+/**
+ * main - main
+ * Return: 0
+ *
+ * Decription: prints _putchar
+ *
+ */
+int main(void)
+{
+	char ll[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int la;
+
+	for (la = 0; la <= 7; la++)
+		_putchar(la);
+
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+funcrions nested loops
