66c024f1f7f985c06f765b3e682e77e262db6d8b
root <jesus4388>
2022-02-07 18:08:35 -0800

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - - description: returns the minutes and hours
+ * return: the minutes and hours
+ *
+ */
+void jack_bauer(void)
+{
+	int hours, minutes
+		;
+	for (hours = 0; hours < 24; hours++)
+	{
+	for (minutes = 0; minutes < 60; minutes++)
+	{
+		_putchar((hours / 10) + 48);
+		_putchar((hours % 10) + 48);
+		_putchar(':');
+		_putchar((minutes / 10) + 48);
+		_putchar((minutes % 10) + 48);
+	}
+	}
+	_putchar('\n');
+}
