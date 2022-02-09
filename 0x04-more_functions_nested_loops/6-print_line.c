7b887b4a8888727129d2375e60a8ded6612ea5b7
root <jesus4388>
2022-02-09 14:41:52 -0800

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - - print a line
+ * @n: number of _
+ * Return:_
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+		_putchar(10);
+	else
+		{
+		for (a = n; a > n; a++)
+			_putchar('_');
+		}
+	_putchar(10);
+}
+
