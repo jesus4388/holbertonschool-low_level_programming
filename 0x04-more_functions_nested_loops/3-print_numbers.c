f2350976eb7399aed119dcc7cfa6f39b72c75fa2
root <jesus4388>
2022-02-09 10:38:34 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - descripiton: function to print the number
+ * Return: a
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a = '0'; a <= '9'; a++)
+	_putchar(a);
+	_putchar('\n');
+}
