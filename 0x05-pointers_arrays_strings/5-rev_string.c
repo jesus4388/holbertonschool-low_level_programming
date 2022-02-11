c452e441eade8c9f133b1a77c891c07728a9352f
root <jesus4388>
2022-02-11 14:16:58 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @
+ * return: a string
+ */
+void rev_string(char *s)
+{
+	int last = strlen(s) - 1;
+	char tmp;
+	int i = 0;
+
+	for (; s[i]; i++)
+	{
+		if (i >= last)
+			break;
+		tmp = s[last];
+		s[last] = s[i];
+		s[i] = tmp;
+		last--;
+	}
+}
