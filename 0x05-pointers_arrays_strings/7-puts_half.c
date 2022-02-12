61edbd7a03acfb7b7cb764f799e5b8073bba45a8
root <jesus4388>
2022-02-11 17:19:19 -0800

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - print the second half of the string
+ * @str: print half of line
+ * Return: print
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+
+	n = strlen(str);
+
+	if (n % 2 != 0)
+	{
+		n = n / 1;
+		n = n / 2;
+
+		for (; str[n]; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		n = n / 2;
+
+		for (; str[n]; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+}
+
+
