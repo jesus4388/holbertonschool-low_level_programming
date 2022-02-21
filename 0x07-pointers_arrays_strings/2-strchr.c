def64cd33031efc2f2380b781f769860cb08dc92
root <jesus4388>
2022-02-21 10:48:14 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,19 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * _strchr
- * @
- * @
+ * _strchr - locates a character
+ * @s: string
+ * @c: character
+ * Return: s
  */
 char *_strchr(char *s, char c)
 {
-	
+	for (;; s++)
+	{
+		if (*s == c)
+			return (s);
+		if (!*s)
+			return ('\0');
+	}
+return ('\0');
 }
