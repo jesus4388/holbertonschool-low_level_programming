4055ed509f0c3c384ed30c0603432fce8ad5f138
root <jesus4388>
2022-02-21 18:08:04 -0800

    chesboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_chessboard -
+ * @a:
+ * Return:
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b;
+	int c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			if (c != 7)
+			{
+				_putchar (a[b][c]);
+			}
+			else
+			{
+				_putchar (a[b][c]);
+				_putchar (10);
+			}
+		}
+	}
+}
