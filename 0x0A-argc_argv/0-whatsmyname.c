f7f24c0195351f3681a1107939a5b8c8a332e8c2
root <jesus4388>
2022-02-25 07:33:33 -0800

    arcv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: 0
+ * @argv: name
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
