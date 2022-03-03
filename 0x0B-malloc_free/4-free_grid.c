a7b267177ecad31b36688f2129b8b5b03123c925
root <jesus4388>
2022-03-02 17:05:24 -0800

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * free_grid - una función que libere una cuadrícula bidimensional
+ * @grid: string
+ * @height: string
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int a = 0;
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
