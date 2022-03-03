ab3da45bb4bcb27964a5a179160a89d26eb38e48
root <jesus4388>
2022-03-03 08:33:39 -0800

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	p = malloc(strlen(s1) + strlen(s2) + 1);
+	p = malloc(strlen(s1) + n + 1);
 	if (p != NULL)
 	{
 		for (i = 0; s1[i]; i++)
