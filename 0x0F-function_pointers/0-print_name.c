0881310fa3767a669ca2b69a6bfe975ecc7da9be
root <jesus4388>
2022-03-09 08:15:38 -0800

    function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
-#include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - print name
  * @name: name
- * @f:
+ * @f: pointer
  * Return: 0
  */
 void print_name(char *name, void (*f)(char *))
