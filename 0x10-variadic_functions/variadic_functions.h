d923f9d223436b1ed1416bc2b9c43766a4a71205
root <jesus4388>
2022-03-10 17:08:08 -0800

    print

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,12 @@
+#include <stdarg.h>
 #ifndef variadic
 #define variadic
+typedef struct op
+{
+	char *c;
+	void (*f)(va_list p);
+} opp;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
