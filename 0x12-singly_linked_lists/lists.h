76d3df6fe4af20b93b192e000fbfe1ed7ec24f1c
root <jesus4388>
2022-03-17 12:12:52 -0700

    list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef nod
+#define nod
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * struct node - struct
+ * @str: char
+ * @len: int
+ * @next: end
+ */
+typedef struct node
+{
+	char *str;
+	int len;
+	struct node *next;
+} list_t;
+size_t print_list(const list_t *h);
+#endif
