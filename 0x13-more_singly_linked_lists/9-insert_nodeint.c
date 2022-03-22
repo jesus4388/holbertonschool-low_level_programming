3f1ac37e8a5bfae59f5d3521e1160fd9ccc52b7f
root <jesus4388>
2022-03-22 16:21:23 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *aux = *head;
 	listint_t *new;
 	
-	new = malloc(sizeof(n));
+	new = malloc(sizeof(listint_t));
 	if (new)
 	{
 		new->n = n;
