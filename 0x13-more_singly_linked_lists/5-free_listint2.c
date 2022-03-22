57b0e3fb3e3912c992c7f0cc903c1ebf18f03417
root <jesus4388>
2022-03-22 07:27:16 -0700

    readme

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *aux1 = *head;
+	listint_t *aux1;
 
 	if (head == NULL || *head == NULL)
 		return;
 
-	for (; aux1;)
+	for (; *head;)
 	{
-		aux1 = aux1->next;
+		aux1 = *head->next;
 		free(*head);
 		*head = aux1;
 	}
