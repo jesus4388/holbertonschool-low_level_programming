d026d736ce1dcc81a20ac20c0b8241047f473060
root <jesus4388>
2022-03-24 11:44:10 -0700

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * binary_to_uint - a function that converts a binary
+ * @b: pointer
+ * Return: 0 or bin
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	unsigned int bin = 0;
+
+	if (b == NULL)
+		return ('0');
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		bin <<= 1;
+		bin = bin + b[i] - '0';
+		i++;
+	}
+	return (bin);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,4 @@
+#ifndef bit
+#define bit
+unsigned int binary_to_uint(const char *b);
+#endif
