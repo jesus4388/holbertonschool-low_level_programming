3b5f43fe9b28043da8e0edb7b1981fd9d35c4df1
root <jesus4388>
2022-03-28 17:51:39 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd < 0)
+	if (fd == -1)
 	{
 		return (-1);
 	}
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	j = strlen(text_content);
 	ret = write(fd, text_content, j);
 
-	if (ret < 0)
+	if (ret == -1)
 	{
 		close(fd);
 		return (-1);
