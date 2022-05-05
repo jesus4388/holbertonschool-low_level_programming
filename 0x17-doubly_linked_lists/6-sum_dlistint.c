5c3e3c1cb4ad3f803648c5c2b4abdd98d35c18fe
root <jesus4388>
2022-05-05 12:48:49 -0700

    add

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,15 @@
 #include "lists.h"
 /**
+ * sum_dlistint - suma
+ * @head: pointer
+ * Return: suma
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
 	if (head == NULL)
-		return(0);
+		return (0);
 	while (head)
 	{
 		sum = sum + head->n;
