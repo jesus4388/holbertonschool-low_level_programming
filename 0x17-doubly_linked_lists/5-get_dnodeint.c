c49a71a5b4c4d499a7741367ce8271e473aec17d
root <jesus4388>
2022-05-05 12:21:22 -0700

    index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - index
+ * @head: pointer
+ * @index: index
+ * Return: node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *aux = head;
+
+	for (i = 0; i < index; i++)
+		aux = aux->next;
+	return (aux);
+}
