31ea0bf0c5c5a991a657965468a079996970b2ab
root <jesus4388>
2022-05-05 12:26:33 -0700

    index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *aux = head;
 
-	for (i = 0; i < index; i++)
-		aux = aux->next;
-	return (aux);
+	if (index)
+	{
+		for (i = 0; i < index; i++)
+			aux = aux->next;
+		return (aux);
+	}
+	else
+		return (NULL);
 }
+
