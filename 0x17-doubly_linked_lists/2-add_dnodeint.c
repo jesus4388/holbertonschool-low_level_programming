2c7659ddbfc09888a4f0d802acf02463785bea05
root <jesus4388>
2022-05-05 09:31:54 -0700

    list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node
+ * @n: entero
+ * @head: head
+ * Return: new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new)
+	{
+		new->n = n;
+		new->next = *head;
+		new->prev = *head;
+		*head = new;
+	}
+	return (new);
+}
