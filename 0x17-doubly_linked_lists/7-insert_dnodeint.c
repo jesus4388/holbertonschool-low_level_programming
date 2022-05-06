37842f306e228b043e084ebaf72930fe8afab040
root <jesus4388>
2022-05-06 15:04:23 -0700

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,9 +18,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->n = n;
 		new->next = NULL;
 		new->prev = NULL;
-		if (*h == NULL)
-			new = *h;
-			return (NULL);
 		if (idx == 0)
 			return (add_dnodeint(h, n));
 		for (i = 0; aux->next; i++)
@@ -30,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (add_dnodeint_end(h, n));
 		for (i = 0; i < idx && aux->next; i++)
 			aux = aux->next;
-		if (i == idx)
+		if (i == idx && aux->next)
 		{
 			aux->prev->next = new;
 			new->prev = aux->prev,
