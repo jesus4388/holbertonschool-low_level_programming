76a6b72545da25f9814247ab14cb638240a4084e
root <jesus4388>
2022-05-05 17:28:01 -0700

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * dlistint_len - len
+ * @h: head
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
+/**
+ * delete_dnodeint_at_index - delete_dnodeint_at_index
+ * @head: pointer
+ * @index: index
+ * Return: 1 or -1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -10,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		n = dlistint_len(*head);
 		if (index == n)
-		{	
+		{
 			while (aux->next)
 			{
 				aux = aux->next;
@@ -19,7 +38,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			free(aux);
 		}
 		for (i = 0; i < index && aux->next; i++)
-        	{
+		{
 			aux = aux->next;
 		}
 		if (index == 0)
@@ -29,14 +48,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			aux->next = NULL;
 			free(aux);
 		}
-		if(index == i && aux->next)
-		{	
+		if (index == i && aux->next)
+		{
 			aux->prev->next = aux->next;
 			aux->next->prev = aux->prev;
 			free(aux);
 		}
 	}
 	else
-		return(-1);
-	return(1);
+		return (-1);
+	return (1);
 }
