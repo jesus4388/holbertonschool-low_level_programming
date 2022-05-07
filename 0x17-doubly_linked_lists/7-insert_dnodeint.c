a95441ee5d8039af7dd10a2863ae332215cbbe9c
root <jesus4388>
2022-05-06 17:06:29 -0700

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,32 +11,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *new;
 	dlistint_t *aux = *h;
-
-	new = malloc(sizeof(dlistint_t));
-	if (new)
+	
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	for (i = 0; aux->next; i++)
+		aux = aux->next;
+	if (i == idx)
+		return (add_dnodeint_end(h, n));
+	if (idx > 0 && *h == NULL)
+		return (NULL);
+	aux = *h;
+	for (i = 0; i < idx && aux->next; i++)
+		aux = aux->next;
+	if (i == idx)
 	{
-		new->n = n;
-		new->next = NULL;
-		new->prev = NULL;
-		if (idx == 0)
-			return (add_dnodeint(h, n));
-		if (idx > 0 && *h == NULL)
-			return (NULL);
-		for (i = 0; aux->next; i++)
-			aux = aux->next;
-		aux = *h;
-		if (idx == i)
-			return (add_dnodeint_end(h, n));
-		for (i = 0; i < idx && aux->next; i++)
-			aux = aux->next;
-		if (i == idx && aux->next)
+		new = malloc(sizeof(dlistint_t));
+		if (new)
 		{
+			new->n = n;
+			new->next = NULL;
+			new->prev = NULL;
+
 			aux->prev->next = new;
 			new->prev = aux->prev,
 			new->next = aux;
 			aux->prev = new;
+			return (new);
 		}
-		return (new);
 	}
 	return (NULL);
 }
--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -13,18 +13,17 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    insert_dnodeint_at_index(&head, 1, 98);
-    add_dnodeint_end(&head, 9);
-    add_dnodeint_end(&head, 6);
-  /**  add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
     add_dnodeint_end(&head, 3);
     add_dnodeint_end(&head, 4);
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);*/
+    add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
-    /**insert_dnodeint_at_index(&head, 0, 4096);*/
+    insert_dnodeint_at_index(&head, 5, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
