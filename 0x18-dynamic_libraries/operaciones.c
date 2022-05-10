5f645844cdfd3bac8b1c01dc9a540ad51b49e780
root <jesus4388>
2022-05-09 18:28:33 -0700

    lib

---
+++ 0x18-dynamic_libraries/operaciones.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * add - add
+ * @a: operator
+ * @b: operator
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
+/**
+ * sub - sub
+ * @a: operator
+ * @b: operator
+ * Return: int
+ */
+int sub(int a, int b)
+{
+        return(a - b);
+}
+/**
+ * div - div
+ * @a: operator
+ * @b: operator
+ * Return: int
+ */
+int div(int a, int b)
+{
+        return(a / b);
+}
+/**
+ * mul - mul
+ * @a: operator
+ * @b: operator
+ * Return: int
+ */int mul(int a, int b)
+{
+        return(a * b);
+}
+/**
+ * mod - mod
+ * @a: operator
+ * @b: operator
+ * Return: int
+ */
+int mod(int a, int b)
+{
+        return(a % b);
+}
