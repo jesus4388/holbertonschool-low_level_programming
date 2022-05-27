b45e383f7350297c6dcd5d9cd9e99d5b0ce5c0ce
root <jesus4388>
2022-05-27 06:55:34 -0700

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,18 @@
 #include "hash_tables.h"
 /**
+ * key_index - key_index
+ * @key: key
+ * @size: size
+ * Return: mod
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index = 0, mod = 0;
 
 	if (size < 1)
-		return(-1);
+		return (-1);
 
 	index = hash_djb2(key);
 	mod = index % size;
-	return(mod);
+	return (mod);
 }
