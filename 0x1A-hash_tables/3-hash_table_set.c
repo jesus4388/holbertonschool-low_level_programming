9ff71ca5ff43aa13c73cf674ca03e9334580ded2
root <jesus4388>
2022-05-27 07:00:05 -0700

    cool

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 /**
+ * hash_table_set - set
+ * @ht: ht
+ * @key: key
+ * @value: value
+ * Return: 1 or 0
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -9,38 +14,38 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (strlen(key) == 0)
 		return (0);
-	
+
 	index = key_index((unsigned char *)key, ht->size);
 	aux = ht->array[index];
 	if (aux == NULL)
 	{
 		new = malloc(sizeof(hash_node_t));
-		if(new)
+		if (new)
 		{
 			new->key = strdup(key);
 			new->value = strdup(value);
 			ht->array[index] = new;
 			new->next = NULL;
-			return(1);
+			return (1);
 		}
 	}
-	while(aux)
+	while (aux)
 	{
 		if (strcmp(aux->key, key) == 0)
 		{
 			aux->value = strdup(value);
-			return(1);
+			return (1);
 		}
 		aux = aux->next;
 	}
 	new = malloc(sizeof(hash_node_t));
-	if(new)
+	if (new)
 	{
 		new->key = strdup(key);
 		new->value = strdup(value);
 		new = aux->next;
 		aux = new;
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
