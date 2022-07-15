f55700d2fff55efb3efd1471799c19686fb25309
jesus4388 <jesushernandez1843@gmail.com>
2022-07-14 23:01:52 -0300

    l

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     flag = 1
     for lis in range(len(grid)):
         for i in range(len(grid[lis])):
-            if flag == 0 and grid[lis][i] == 1:
-                if grid[lis][i] == 1:
-                    x += 2
-                if grid[lis - 1][i] == 1 and grid[lis][i + 1] == 1 and grid[lis + 1][i] == 1:
-                    x -= 1
-            if grid[lis][i] == 1 and flag == 1:
-                x = 4
-                flag = 0
+            if grid[lis][i] == 1:
+                x += 4
+                if grid[lis -1]:
+                    if grid[lis - 1][i] == 1:
+                        x -= 2
+                if grid[lis][i -1]:
+                    if grid[lis][i - 1] == 1:
+                        x -= 2
     return x
